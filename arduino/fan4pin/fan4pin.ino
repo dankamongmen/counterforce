25c35198c9d88496b2690d64cb09ea5e76436654
nick black <dankamongmen@gmail.com>
2022-08-17 01:33:03 -0400

    use arduino pwm library for fan control

--- arduino/fan4pin/fan4pin.ino
+++ arduino/fan4pin/fan4pin.ino
@@ -1,67 +1,39 @@
+// intended to run on a Mega2560
+#include <PWM.h>
+
 volatile unsigned Pulses; // counter for input events, reset each second
 
-const int RPMPIN = 2; // pin connected to tachometer
-//const int PWMPIN = 9; // pin connected to PWM
+// we need an interrupt-capable pin. on MEGA, this is
+// 2, 3, 18, 19, 20, 21 (last two conflict with i2c)
+const int RPMPIN = 21; // pin connected to tachometer
+// on mega:
+//  pin 13, 4 == timer 0 (used for micros())
+//  pin 12, 11 == timer 1
+//  pin 10, 9 == timer 2
+//  pin 5, 3, 2 == timer 3
+//  pin 8, 7, 6 == timer 4
+const int PWMPIN = 9; // pin connected to PWM
 
 static void rpm(){
-  Pulses++;
+  if(Pulses < 65535){
+    ++Pulses;
+  }
 }
-
-/*
-//configure Timer 1 (pins 9,10) to output 25kHz PWM
-static void setupTimer1(){
-    //Set PWM frequency to about 25khz on pins 9,10 (timer 1 mode 10, no prescale, count to 320)
-    TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
-    TCCR1B = (1 << CS10) | (1 << WGM13);
-    ICR1 = 320;
-    OCR1A = 0;
-    OCR1B = 0;
-}
-
-//configure Timer 2 (pin 3) to output 25kHz PWM. Pin 11 will be unavailable for output in this mode
-static void setupTimer2(){
-    //Set PWM frequency to about 25khz on pin 3 (timer 2 mode 5, prescale 8, count to 79)
-    TIMSK2 = 0;
-    TIFR2 = 0;
-    TCCR2A = (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
-    TCCR2B = (1 << WGM22) | (1 << CS21);
-    OCR2A = 79;
-    OCR2B = 0;
-}
-
-//equivalent of analogWrite on pin 9
-static void setPWM1A(float f){
-    f=f<0?0:f>1?1:f;
-    OCR1A = (uint16_t)(320*f);
-}
-
-//equivalent of analogWrite on pin 10
-static void setPWM1B(float f){
-    f=f<0?0:f>1?1:f;
-    OCR1B = (uint16_t)(320*f);
-}
-
-//equivalent of analogWrite on pin 3
-static void setPWM2(float f){
-    f=f<0?0:f>1?1:f;
-    OCR2B = (uint8_t)(79*f);
-}
-*/
 
 void setup(){
   Serial.begin(115200);
-  pinMode(RPMPIN, INPUT);
+  InitTimersSafe(); // don't blow away timer 0
+  if(!SetPinFrequency(PWMPIN, 25000)){
+    Serial.println("Failure initializing PWMPIN");
+  }else{
+    Serial.print("Successfully initialized PWMPIN at resolution ");
+    pinMode(PWMPIN, OUTPUT);
+    float res = GetPinResolution(PWMPIN);
+    Serial.println(res);
+    pwmWrite(PWMPIN, 128);
+  }
+  pinMode(RPMPIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(RPMPIN), rpm, RISING);
-  /*
-  //enable outputs for Timer 1
-  pinMode(PWMPIN, OUTPUT); //1A
-  //pinMode(10,OUTPUT); //1B
-  //setupTimer1();
-  //enable outputs for Timer 2
-  //pinMode(3,OUTPUT); //2
-  //setupTimer2();
-  //note that pin 11 will be unavailable for output in this mode!
-  */
 }
  
 void loop (){
@@ -69,23 +41,22 @@ void loop (){
   unsigned long m = micros();
   unsigned long cur;
 
-/*
-  for(int i=0; i<255; i++){
-    analogWrite(PWMPIN, i);
-    delay(500);
-  }
- 
-  //setPWM1A(0.4f); //set duty to 50% on pin 9
-  //setPWM1B(0.4f); //set duty to 50% on pin 10
-  //setPWM2(0.4f); //set duty to 80% on pin 3
-  */
-
   sei();       // enable interrupts
   do{
     cur = micros();
+    if(cur <= 0){
+      Serial.print("NEGATIVE MICROS!\n"); // did you fuck timer 0?
+    }
   }while(cur - m < 1000000); // FIXME handle overflow
   cli();       // disable interrupts
-  int c = Pulses * 30;
+  unsigned c = Pulses * 30;
+  Serial.print(RPMPIN, DEC);
+  Serial.print(" ");
+  Serial.print(Pulses, DEC);
+  Serial.print(" ");
   Serial.print(c, DEC);
-  Serial.print(" rpm gunk\r\n");
+  Serial.print(" rpm ");
+  Serial.print(cur - m, DEC);
+  Serial.println("µs");
+  pwmWrite(PWMPIN, 128);
 }
