cd774850d9c04818182b61de64bb4a8f2abb1111
nick black <dankamongmen@gmail.com>
2022-08-23 18:09:11 -0400

    geiger: convert to esp32 from arduino

---
+++ esp32/geiger/geiger.ino
@@ -0,0 +1,55 @@
+// use a Heltect ESP32-LoRAv2 to read from RHElectronics geiger counter and
+// KY-038 sound level sensor. the former needs 5V; the latter can get by with
+// our native 3.3V.
+
+#define RADPIN 36
+#define MICPIN 39 // analog input
+
+// we want a sample per second
+#define SAMPLEMS 1000
+
+#include "heltec.h"
+
+volatile unsigned long count; // disintegration events this quantum
+unsigned long previousMillis; // variable for time measurement
+
+void tube_impulse(){
+  ++count;
+}
+
+void setup(){
+  count = 0;
+  Heltec.begin(true /*DisplayEnable Enable*/,
+               false /*LoRa Disable*/,
+               true /*Serial Enable*/);
+  Heltec.display->setFont(ArialMT_Plain_10);
+  pinMode(RADPIN, INPUT);     // set pin INT0 input for capturing GM Tube events
+  digitalWrite(RADPIN, HIGH); // turn on internal pullup resistors, solder C-INT on the PCB
+  attachInterrupt(digitalPinToInterrupt(RADPIN), tube_impulse, FALLING); // define external interrupts
+  pinMode(MICPIN, INPUT);
+  previousMillis = 0;
+}
+
+void loop(){
+  unsigned long now = millis();
+  // need to keep everything unsigned long so that we properly
+  // handle overflow of millis() at ~50 days
+  if(now - previousMillis > SAMPLEMS){
+    previousMillis = now;
+    //Serial.print("Count: ");
+    //Serial.println(count);
+    count = 0;
+    float a = analogRead(MICPIN);
+    //Serial.print("Mic: ");
+    Serial.println(a);
+    Heltec.display->clear();
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->drawString(0, 0, "Disintegrations: ");
+    Heltec.display->drawString(77, 0, String(count));
+    Heltec.display->drawString(0, 11, "Noise: ");
+    Heltec.display->drawString(35, 11, String(a));
+    Heltec.display->drawString(0, 21, "Uptime: ");
+    Heltec.display->drawString(40, 21, String(millis() / 1000));
+    Heltec.display->display();
+  }
+}
