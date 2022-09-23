caeff823437b35378fc84125caa265f93e92b6e3
nick black <dankamongmen@gmail.com>
2022-09-23 05:51:20 -0400

    send up it87 rpm sensors

--- codi6/codi6.ino
+++ codi6/codi6.ino
@@ -1,17 +1,18 @@
 #include <FastLED.h>
-#define NUM_STRIPS 3
+#define NUM_STRIPS 6
 #define NUM_LEDS_PER_STRIP (37 * 2)
 #define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
  
 CRGB leds[NUM_LEDS];
  
-// For mirroring strips, all the "special" stuff happens just in setup.  We
-// just addLeds multiple times, once for each strip
 void setup() {
   // tell FastLED there's 15  NEOPIXEL leds on pin 3, starting at index 0 in the led array
   FastLED.addLeds<NEOPIXEL, 3>(leds, 0, NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<NEOPIXEL, 4>(leds, 0, NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<NEOPIXEL, 5>(leds, 0, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 9>(leds, NUM_LEDS_PER_STRIP * 2, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 12>(leds, 0, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 15>(leds, NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 18>(leds, NUM_LEDS_PER_STRIP * 2, NUM_LEDS_PER_STRIP);
 }
 
 void loop() {
