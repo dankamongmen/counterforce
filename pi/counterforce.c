a14a75047e6b0471122375d08761f869351fe13a
nick black <dankamongmen@gmail.com>
2022-09-23 08:36:52 -0400

    [sensors] grab liquidctl fan data

--- pi/counterforce.c
+++ pi/counterforce.c
@@ -16,12 +16,24 @@ int main(void){
     return EXIT_FAILURE;
   }
   struct ncvisual_options mopts = {0};
+  mopts.blitter = NCBLIT_PIXEL;
+  ncvgeom mgeom;
+  if(ncvisual_geom(nc, mobo, &mopts, &mgeom)){
+    notcurses_stop(nc);
+    return EXIT_FAILURE;
+  }
   struct ncplane* mobop = ncvisual_blit(nc, mobo, &mopts);
   if(mobop == NULL){
     notcurses_stop(nc);
     return EXIT_FAILURE;
   }
   struct ncvisual_options popts = {0};
+  popts.blitter = NCBLIT_PIXEL;
+  ncvgeom pgeom;
+  if(ncvisual_geom(nc, psu, &popts, &pgeom)){
+    notcurses_stop(nc);
+    return EXIT_FAILURE;
+  }
   struct ncplane* psup = ncvisual_blit(nc, psu, &popts);
   if(psup == NULL){
     notcurses_stop(nc);
@@ -31,6 +43,7 @@ int main(void){
     notcurses_stop(nc);
     return EXIT_FAILURE;
   }
+  sleep(4);
   // FIXME loop, showing data
   notcurses_stop(nc);
   return EXIT_SUCCESS;
