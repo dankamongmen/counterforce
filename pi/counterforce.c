08481278b2c30146f4d67acad917ba95613f9d2c
nick black <dankamongmen@gmail.com>
2022-09-23 03:59:52 -0400

    add terrible sensors-collect draft to get some k10 data

--- pi/counterforce.c
+++ pi/counterforce.c
@@ -5,6 +5,18 @@ int main(void){
   if(nc == NULL){
     exit(EXIT_FAILURE);
   }
+  struct ncvisual* mobo = ncvisual_from_file("counterforce-mobo.jpg");
+  if(mobo == NULL){
+    notcurses_stop(nc);
+    return EXIT_FAILURE;
+  }
+  if(psu == NULL){
+    notcurses_stop(nc);
+    return EXIT_FAILURE;
+  }
+  struct ncvisual* psu = ncvisual_from_file("counterforce-psu.jpg");
+  // FIXME render them
+  // FIXME loop, showing data
   notcurses_stop(nc);
   return EXIT_SUCCESS;
 }
