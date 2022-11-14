2dc7c8d97e7eb42148c0fc3cfe3a673b2eff79c1
nick black <dankamongmen@gmail.com>
2022-11-14 06:09:06 -0500

    [esp8266] back to D2 for 1wire

--- esp8266/fanmgr/fanmgr.ino
+++ esp8266/fanmgr/fanmgr.ino
@@ -12,7 +12,7 @@
 // only one ADC on the ESP8266
 const int TEMPPIN = A0; // coolant thermistor (2-wire)
 // ambient temperature (digital thermometer, Dallas 1-wire)
-const int AMBIENTPIN = D7;
+const int AMBIENTPIN = D2;
 
 // PWM/tach pins for fans and pumps
 const int PUMPPWMPIN = D5;
