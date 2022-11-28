aa76d75b11a58037c7ef27ad8e719ed2de286ee7
nick black <dankamongmen@gmail.com>
2022-11-27 23:47:11 -0500

    [esp8266] use ESP8266_pwm_new library

--- esp8266/fanmgr/fanmgr.ino
+++ esp8266/fanmgr/fanmgr.ino
@@ -8,7 +8,10 @@
 #include <ArduinoJson.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <pwm.h>
 #include "common.h"
+
+#define PWM_CHANNELS 2
 
 // only one ADC on the ESP8266
 const int TEMPPIN = A0; // coolant thermistor (2-wire)
@@ -38,15 +41,29 @@ EspMQTTClient client(
 OneWire twire(AMBIENTPIN);
 DallasTemperature digtemp(&twire);
 
+// esp8266_pwm_new uses 200ns units. we want 25KHz, which is 40us,
+// which is 40k ns, which is 200x 200ns units.
+#define PWMPERIOD 200
+
 void setup(){
   Serial.begin(115200);
   Serial.print("booting esp8266 ");
   Serial.println(VERSION);
   client.enableDebuggingMessages();
   client.enableMQTTPersistence();
-  analogWriteFreq(25000);
   pinMode(PUMPPWMPIN, OUTPUT);
   pinMode(FANPWMPIN, OUTPUT);
+  uint32_t cycle = PWMPERIOD / 2; // half duty to start
+  uint32_t cycles[PWM_CHANNELS] = { cycle, cycle, };
+  uint32_t pwms[PWM_CHANNELS][3] = {
+    {PERIPHS_IO_MUX_MTDI_U,  FUNC_GPIO12, 12},
+	  {PERIPHS_IO_MUX_MTMS_U,  FUNC_GPIO14, 14},
+  };
+  pwm_init(PWMPERIOD, cycles, sizeof(pwms) / sizeof(*pwms), pwms);
+  pwm_start();
+  /*
+  analogWriteFreq(25000);
+  */
   set_pwm(INITIAL_FAN_PWM);
   set_pump_pwm(INITIAL_PUMP_PWM);
   pinMode(TEMPPIN, INPUT);
@@ -55,7 +72,8 @@ void setup(){
 
 // set up the desired PWM value
 static int set_pwm(unsigned p){
-  analogWrite(FANPWMPIN, p);
+  //analogWrite(FANPWMPIN, p);
+  pwm_set_duty(PWMPERIOD * p / 255, 1);
   Serial.print("configured fan PWM: ");
   Serial.println(p);
   Pwm = p;
@@ -64,7 +82,8 @@ static int set_pwm(unsigned p){
 
 // set up the desired pump PWM value
 static int set_pump_pwm(unsigned p){
-  analogWrite(PUMPPWMPIN, p);
+  //analogWrite(PUMPPWMPIN, p);
+  pwm_set_duty(PWMPERIOD * p / 255, 2);
   Serial.print("configured pump PWM: ");
   Serial.println(p);
   PumpPwm = p;
--- espcommon/fanmgr/common.h
+++ espcommon/fanmgr/common.h
@@ -1,7 +1,5 @@
 // common routines for the ESP32 and ESP8266 implementations of fanmgr
-#include "EspMQTTClient.h"
-
-#define VERSION "v2.1.1"
+#define VERSION "v2.2.0"
 
 #ifdef ESP32
 #define ISR IRAM_ATTR
