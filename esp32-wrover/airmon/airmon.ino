199c05cd8f4791479fce5f39bac36c1f350c2c7a
nick black <dankamongmen@gmail.com>
2024-06-13 12:36:29 -0400

    more airmon

--- esp32-wrover/airmon/airmon.ino
+++ esp32-wrover/airmon/airmon.ino
@@ -29,22 +29,22 @@ void setup(void){
   Serial.begin(115200);
   adc1_config_width(ADC_WIDTH_BIT_12);
   // 37 and 38 (channels 1 and 2) are unavailable on ESP32-WROOM.
-  init_adc_chan("H2S", ADC1_CHANNEL_0);
-  init_adc_chan("CH4", ADC1_CHANNEL_3);
-  init_adc_chan("VOC", ADC1_CHANNEL_4);
-  init_adc_chan("unspec", ADC1_CHANNEL_5);
+  init_adc_chan("VOC", ADC1_CHANNEL_0);
+  init_adc_chan("CH4", ADC1_CHANNEL_4);
+  init_adc_chan("H2S", ADC1_CHANNEL_5);
+  init_adc_chan("unspec", ADC1_CHANNEL_3);
   init_adc_chan("unspec", ADC1_CHANNEL_6);
   init_adc_chan("unspec", ADC1_CHANNEL_7);
 }
 
 void loop(void){
-  int h2s = adc1_get_raw(ADC1_CHANNEL_0);
-  int ch4 = adc1_get_raw(ADC1_CHANNEL_3);
-  int voc = adc1_get_raw(ADC1_CHANNEL_4);
+  int voc = adc1_get_raw(ADC1_CHANNEL_0);
+  int ch4 = adc1_get_raw(ADC1_CHANNEL_4);
+  int h2s = adc1_get_raw(ADC1_CHANNEL_5);
   printf("H2S: %d CH4: %d VOC: %d\n", h2s, ch4, voc);
-  int u1 = adc1_get_raw(ADC1_CHANNEL_5);
+  int u1 = adc1_get_raw(ADC1_CHANNEL_3);
   int u2 = adc1_get_raw(ADC1_CHANNEL_6);
   int u3 = adc1_get_raw(ADC1_CHANNEL_7);
-  printf("unspec: %d %d %d\n", u1, u2, u3);
+  printf("H2S: %d u2: %d u3: %d\n", u1, u2, u3);
   delay(1000);
 }
