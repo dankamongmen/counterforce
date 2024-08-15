a18ff5e313d250751cb70a8e2ba4942de9b43d74
nick black <dankamongmen@gmail.com>
2024-08-15 09:18:51 -0400

    bamboozle: print uptime on SSD1306

--- unor4/airmon/airmon.ino
+++ unor4/airmon/airmon.ino
@@ -79,7 +79,23 @@ int displayCoreSetup(void){
   return 0;
 }
 
-#define TEXTHEIGHT 10
+void maketimestr(char *str){
+  uint64_t ticks = millis();
+  ticks /= 1000;
+  unsigned s = ticks % 60;
+  unsigned m = (ticks % 3600) / 60;
+  unsigned h = (ticks % 86400lu) / 3600;
+  unsigned d = ticks / 86400lu;
+  if(d){
+    sprintf(str, "%ud %uh %um %us", d, h, m, s);
+  }else if(h){
+    sprintf(str, "%uh %um %us", h, m, s);
+  }else if(m){
+    sprintf(str, "%um %us", m, s);
+  }else{
+    sprintf(str, "%us", s);
+  }
+}
 
 int displayDraw(float ambient){
   if(!usingDisplay){
@@ -93,10 +109,15 @@ int displayDraw(float ambient){
   display.setCursor(0, 0);
   display.println("bambooster v" VERSION);
   if(isnan(ambient)){
-    display.print(DEVNAME " --");
+    display.println(DEVNAME " --");
   }else{
-    //display.print(DEVNAME " %0.2f C", ambient);
+    display.print(DEVNAME " ");
+    display.println((int)ambient); // FIXME float
   }
+  char tempstr[16];
+  maketimestr(tempstr);
+  display.print("uptime: ");
+  display.println(tempstr);
   display.display();
   return 0;
 }
@@ -176,7 +197,6 @@ void setup(){
   }
   setup_interrupt(TACH_PIN);
   pinMode(PWM_PIN, OUTPUT);
-  pinMode(TEMP_PIN, INPUT);
   pinMode(RELAY_PIN, OUTPUT);
   pwmd3.begin(25000.0f, 0.0f);
 }
@@ -202,12 +222,39 @@ bool deltaTenSeconds(unsigned long l, unsigned long now){
   return delta(l, now) > 10000;
 }
 
+void get_temp(float *temp){
+  static bool onewire_connected;
+
+  *temp = NAN;
+  if(!onewire_connected){
+    if(connect_onewire(&digtemp) == 0){
+      onewire_connected = true;
+      uint8_t addr;
+      if(digtemp.getAddress(&addr, 0)){
+        Serial.print("digtemp 0 address: ");
+        Serial.println(addr);
+      }
+      uint8_t res, dev;
+      dev = 0;
+      res = digtemp.getResolution(&dev);
+      printf("therm resolution: %u bits\n", res);
+      if(digtemp.setResolution(&dev, 9)){
+        printf("set resolution to 9 bits\n");
+      }
+    }
+  }
+  if(onewire_connected){
+    if(readAmbient(temp, &digtemp)){
+      onewire_connected = false;
+    }
+  }
+}
+
 void loop(){
   // last time we attempted to connect to WiFi. we hold off for ten
   // seconds following a failed attempt, as it is a blocking call.
   static unsigned long lastWifiAttempt;
   static unsigned long lastTachSample;
-  static bool onewire_connected;
   static bool wifiup;
   float ambient_temp = NAN;
 
@@ -259,29 +306,7 @@ void loop(){
       }
     }
   }
-  if(!onewire_connected){
-    if(connect_onewire(&digtemp) == 0){
-      onewire_connected = true;
-      uint8_t addr;
-      if(digtemp.getAddress(&addr, 0)){
-        Serial.print("digtemp 0 address: ");
-        Serial.println(addr);
-      }
-      uint8_t res, dev;
-      dev = 0;
-      res = digtemp.getResolution(&dev);
-      printf("therm resolution: %u bits\n", res);
-      if(digtemp.setResolution(&dev, 9)){
-        printf("set resolution to 9 bits\n");
-      }
-    }
-  }
-  if(onewire_connected){
-    if(readAmbient(&ambient_temp, &digtemp)){
-      onewire_connected = false;
-      ambient_temp = NAN;
-    }
-  }
+  get_temp(&ambient_temp);
   mqttmsg m(client);
   m.publish();
   for(unsigned i = 0 ; i < sizeof(sensors) / sizeof(*sensors) ; ++i){
@@ -289,6 +314,7 @@ void loop(){
   }
   Serial.print("wifi rssi: ");
   Serial.println(WiFi.RSSI());
+  ambient_temp = rand();
   displayDraw(ambient_temp);
   delay(1000);
 }
